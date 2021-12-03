83e8ca05c6b96f059a4b88a0d9224b0eaa9c45a9
Alfie Lockrey <47219621+AlfieLockrey@users.noreply.github.com>
2021-12-03 11:57:10 +0000

    Created DoIt Function
    Started Variable Creation

--- LGG Model.py
+++ LGG Model.py
@@ -4,3 +4,9 @@ Created on Fri Dec  3 11:44:15 2021
 
 @author: Group 47
 """
+import numpy as np
+D_c = 100e-3 # Diameter of the Combustion Chamber
+
+A_c = np.pi * (D_c/2)**2
+
+def DoIt(A_c=A_c, )
