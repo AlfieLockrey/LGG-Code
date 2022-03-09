ab6a913aad241a04048e53c3816291da232beafa
AlfieLockrey <47219621+AlfieLockrey@users.noreply.github.com>
2022-03-09 21:16:55 +0000

    Updated gun parameters
    Possible to exceed 9.8km/s if P0_pt is increased

--- LGG Model.py
+++ LGG Model.py
@@ -31,10 +31,10 @@ plt.rcParams['figure.dpi'] = 250
 
 
 # COMBUSTION CHAMBER VALUES
-D_c = 30e-3                     # Diameter of the Combustion Chamber
-L0_c = 15e-3                    # Length of the Combustion Chamber
+D_c = 25e-3                     # Diameter of the Combustion Chamber
+L0_c = 150e-3                    # Length of the Combustion Chamber
 P0_c = 100e3                    # Initial pressure in the Combustion Chamber (before detonation)
-C = 8e-3                       # Charge Mass 5 to 20 g
+C = 25e-3                       # Charge Mass 5 to 20 g
 
 gamma_c = 1.2238                # γ Gamma for the combustion products
 
@@ -48,23 +48,23 @@ CoVolume_propel = 0.8e-3        # η Propellant Co-Volum
 V0_c = L0_c * math.pi * (D_c / 2)**2
 
 # PISTON VALUES
-D_pis = 30e-3            # Diameter of piston
+D_pis = 25e-3            # Diameter of piston
 m_pis = 110e-3          # Mass of the piston
 mu_static_pis = 0       # Coefficient of friction for the piston against the pump tube
 mu_dynamic_pis = 0
 allowPistonRearward = False
 
 # PUMP TUBE VALUES
-P0_pt = 10e6       # Initial pressure in the pump tube (ahead of the piston)
-L0_pt = 1.3        # Length of the pump tube
-D_pt = 30e-3        # Diameter of the pump tube
+P0_pt = 30e6       # Initial pressure in the pump tube (ahead of the piston)
+L0_pt = 1.5        # Length of the pump tube
+D_pt = D_pis        # Diameter of the pump tube
 gamma_lg = 1.66     # Gamma for the light gas
 
 # RUPTURE DISK VALUES
-P_rupt = 35e+6  # Pressure at which the rupture disk ruptures
+P_rupt = 150e+6  # Pressure at which the rupture disk ruptures
 
 # BARREL VALUES
-L_b = 1.9           # Length of the barrel
+L_b = 5           # Length of the barrel
 D_b = 12.7e-3       # Diameter of the barrel
 P0_b = 133          # Initial upstream pressure in the barrel
 
@@ -73,12 +73,12 @@ gamma_ic = 1.4      # Gamma for the (near vaccum) gas in the impact chamber
 V_ic = 1
 
 # PROJECTILE & SABOT VALUES
-m_pr = 17e-3    # Projectile mass
-m_sb = 0        # Sabot Mass0.47e-3
+m_pr = 1e-3    # Projectile mass
+m_sb = .45e-3        # Sabot Mass0.47e-3
 mu_sb = 0     # Sabot Friction coeff.
 
 # SIMULATION DETAILS
-delta_t = 1e-6  # Time step length
+delta_t = 1e-7  # Time step length
 
 # Calculating areas from diameters
 A_c = np.pi * (D_c / 2)**2
@@ -348,6 +348,7 @@ def DoIt(A_c=A_c, L0_c=L0_c, P0_c=P0_c, gamma_c=gamma_c, A_pis=A_pis,
         if i * delta_t > .01:                 # If projectile hasnt left the barrel after .5 seconds then something is wrong
             n_disk_rupture = 0
             t_disk_rupture = 0
+            print('Projectile failed to leave the barrel. Rupture disk pressure was probably not exceeded.')
             break
 
     print('Projectile Exit Velocity = ', round(v_pr_array[-1], 3), 'mps')
